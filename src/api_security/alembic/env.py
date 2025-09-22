d7e759cafdeb958866fc098cfb1d58ca1ac28b20
MohanadElMetwally <mohanadelmetwally@gmail.com>
2025-09-22 20:31:59 +0300

    feat(alembic): Add env

---
+++ src/api_security/alembic/env.py
@@ -0,0 +1,53 @@
+from logging.config import fileConfig
+
+from alembic import context
+
+from api_security.core.config import settings
+from api_security.models import Base
+
+# this is the Alembic Config object, which provides
+# access to the values within the .ini file in use.
+config = context.config
+connection = settings.SQLALCHEMY_DATABASE_URI
+escaped_at_symbol_connection = connection.replace("%", "%%")
+config.set_main_option("sqlalchemy.url", escaped_at_symbol_connection)
+
+# Interpret the config file for Python logging.
+# This line sets up loggers basically.
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
+
+# add your model's MetaData object here
+# for 'autogenerate' support
+# from myapp import mymodel
+# target_metadata = mymodel.Base.metadata
+target_metadata = Base.metadata  # type: ignore
+
+# other values from the config, defined by the needs of env.py,
+# can be acquired:
+# my_important_option = config.get_main_option("my_important_option")
+# ... etc.
+
+
+def run_migrations_offline() -> None:
+    """Run migrations in 'offline' mode.
+
+    This configures the context with just a URL
+    and not an Engine, though an Engine is acceptable
+    here as well.  By skipping the Engine creation
+    we don't even need a DBAPI to be available.
+
+    Calls to context.execute() here emit the given string to the
+    script output.
+
+    """
+    url = config.get_main_option("sqlalchemy.url")
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
