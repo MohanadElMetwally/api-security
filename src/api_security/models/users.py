7c2986b11a2fe2d7ef4b1fd2ef2c90b801385b26
MohanadElMetwally <mohanadelmetwally@gmail.com>
2025-09-24 13:06:28 +0300

    feat(models): Add username and email to Users

--- src/api_security/models/users.py
+++ src/api_security/models/users.py
@@ -1,16 +1,16 @@
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-from api_security._typing.sqla import bit0, bit1
+from api_security._typing.sqla import bit0, bit1, vstr, vstr255
 from api_security.core.enums.roles import UserRoles
 from api_security.models import Base
 from api_security.models.base import PkIntIdMixin
 
 
 class Users(Base, PkIntIdMixin):
-    full_name: Mapped[str]
+    full_name: Mapped[vstr]
     role: Mapped[UserRoles]
-    username: Mapped[str] = mapped_column(unique=True)
-    email: Mapped[str] = mapped_column(unique=True)
+    username: Mapped[vstr255] = mapped_column(unique=True)
+    email: Mapped[vstr255] = mapped_column(unique=True)
     hashed_password: Mapped[str]
     is_active: Mapped[bit1]
     is_deleted: Mapped[bit0]
