3888a984c45ee5f610213b5319e45c46b7d9d256
MohanadElMetwally <mohanadelmetwally@gmail.com>
2025-09-24 13:04:44 +0300

    feat(types): Add vstr255 and vstr types

--- src/api_security/_typing/sqla.py
+++ src/api_security/_typing/sqla.py
@@ -1,6 +1,9 @@
 from typing import Annotated
 
+from sqlalchemy import VARCHAR
 from sqlalchemy.orm import mapped_column
 
 bit0 = Annotated[bool, mapped_column(server_default="0")]
 bit1 = Annotated[bool, mapped_column(server_default="1")]
+vstr = Annotated[str, mapped_column(VARCHAR)]
+vstr255  = Annotated[str, mapped_column(VARCHAR(255))]
