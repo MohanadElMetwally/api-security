0650dbedac2c7dc5df01ecc8ebfbdaf35bb2602f
MohanadElMetwally <mohanadelmetwally@gmail.com>
2025-11-06 11:20:45 +0200

    refactor(core): adjust jwt token to include id of user only in payload

--- src/api_security/core/security.py
+++ src/api_security/core/security.py
@@ -4,20 +4,16 @@ import jwt
 from passlib.context import CryptContext
 
 from api_security.core.config import settings
-from api_security.models.users import Users
-from api_security.schemas.base import UserInfo
 
 ALGORITHM = "HS256"
 pwd_context = CryptContext(schemes="bcrypt", deprecated="auto")
 
 
-def create_access_token(user: Users, expire_mins: timedelta) -> str:
+def create_access_token(sub: int, expire_mins: timedelta) -> str:
     expire = datetime.now(UTC) + expire_mins
-    jwt_subject = UserInfo.model_validate(user)
     to_encode = {
         "exp": expire,
-        "sub": str(jwt_subject.id),
-        **jwt_subject.model_dump(exclude={"id"})
+        "sub": str(sub),
     }
     encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, ALGORITHM)
     return encoded_jwt
